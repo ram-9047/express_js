850691ffe7f82eb6f96331a6b3acfb0ae1b8370d
ram-9047 <ramg786@live.com>
2022-08-22 18:37:35 +0530

    adds size input box

--- app.js
+++ app.js
@@ -1,20 +1,35 @@
-
 const express = require("express");
-
+// const add_product = require("./add_product.html");
+const bodyParser = require("body-parser");
 const port = 8000;
 
 const app = express();
 
+//---------body parsing
+app.use(bodyParser.urlencoded({ extended: false }));
+
 //----------middleware
-app.use((req, res, next) => {
-  console.log("in the middleware");
-  next();
+app.use("/add-product", (req, res, next) => {
+  console.log("in add-product");
+  res.sendFile("add_product.html", {
+    root: __dirname,
+  });
+  // res.send(`<form action="/product" method="POST">
+  // <input type="text" name ="title"/>
+  // <button type="submit">Add Item</button>
+  // </form>`);
 });
-app.use((req, res, next) => {
-  console.log("in another middleware");
-  res.send("<h1>Good old Days</h1>");
+
+app.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
+  console.log("in the root middleware");
+  res.send("<h1>Hello from express</h1>");
 });
 
 app.listen(port, () => {
-  `server is listening at ${port}`;
+  console.log(`server is listening at ${port}`);
 });
